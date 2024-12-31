a5870dae031d71b47c060539deb14f9bc259af5d
Nathanael Hondi <latest_nhondi@hotmail.com>
2024-12-31 05:51:14 +0000

    Loading Screen + Main Menu Setup
    Loading Screen
    Art for Loading Screen
    
    Co-Authored-By: mekhtiari-cpu <182498067+mekhtiari-cpu@users.noreply.github.com>

---
+++ Assets/PersistentManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class to handle scene transitions and constant data throughout all the scenes
+public class PersistentManager : MonoBehaviour
+{
+    public static PersistentManager Instance;
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LoadingScreenManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenManager : MonoBehaviour
+{
+    private string currentActiveScene;
+    private const string LOADINGSCENENAME = "Loading Scene";
+    private const string MAINMENUSCENE = "Main Menu";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Load Main Menu Scene on start
+        LoadSceneWithTransition(MAINMENUSCENE);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // We Call this function for any scene transitions @maryam.ekihtiari. This should allow use to use the loading scene to appear inbetween scenes
+    public void LoadSceneWithTransition(string sceneName)
+    {
+        StartCoroutine(LoadSceneRoutine(sceneName));
+    }
+
+    private IEnumerator LoadSceneRoutine(string newSceneName)
+    {
+        currentActiveScene = SceneManager.GetActiveScene().name;
+
+        if (!SceneManager.GetSceneByName(LOADINGSCENENAME).isLoaded)
+        {
+            SceneManager.LoadScene(LOADINGSCENENAME, LoadSceneMode.Additive);
+        }
+
+        // Begin async loading
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(newSceneName, LoadSceneMode.Additive);
+        asyncLoad.allowSceneActivation = false;
+
+        // Wait for the scene to fully load
+        while (!asyncLoad.isDone)
+        {
+            if (asyncLoad.progress >= 0.9f) // Scene is loaded but not yet activated
+            {
+                // Activate the scene
+                asyncLoad.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+
+        // Switch the active scene
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(newSceneName));
+
+        // Unload the previous active scene if applicable
+        if (!string.IsNullOrEmpty(currentActiveScene) && currentActiveScene != LOADINGSCENENAME)
+        {
+            SceneManager.UnloadSceneAsync(currentActiveScene);
+        }
+
+        currentActiveScene = newSceneName;
+    }
+}
